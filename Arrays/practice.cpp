7c9f48b0c2a818921b2870a1c7661b2350044386
Omprakash <omprakashpandit524@gmail.com>
2023-01-18 19:34:00 +0530

    DSA Arrays

---
+++ Arrays/practice.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int mark[100];
+
+    cout<<"Enter no.of student : ";
+    int n;cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>mark[i];
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<mark[i]<<" ";
+    }
+    return 0;
+}
